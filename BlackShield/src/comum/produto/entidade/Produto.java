7b5be076f314b8a05e8152a6efc8b9b829c4a12b
beatrizChagas <beatriznery.12@gmail.com>
2014-11-07 08:58:57 -0300

    Entidade Produto
    inclusão getters and setters

--- BlackShield/src/comum/produto/entidade/Produto.java
+++ BlackShield/src/comum/produto/entidade/Produto.java
@@ -6,5 +6,31 @@ public class Produto {
 	private String nome;
 	private String descricao;
 	private double custo;
+	public Integer getIdProduto() {
+		return idProduto;
+	}
+	public void setIdProduto(Integer idProduto) {
+		this.idProduto = idProduto;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public double getCusto() {
+		return custo;
+	}
+	public void setCusto(double custo) {
+		this.custo = custo;
+	}
+	
+	
 
 }
