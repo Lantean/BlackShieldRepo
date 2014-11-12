9db8932c0af4269e9de40dfb9889904498c22925
beatrizChagas <beatriznery.12@gmail.com>
2014-11-11 23:37:39 -0300

    libs hibernate

--- BlackShield/src/comum/compra/entidade/Compra.java
+++ BlackShield/src/comum/compra/entidade/Compra.java
@@ -1,13 +1,40 @@
 package comum.compra.entidade;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.Entity;
+
 import comum.produto.entidade.Produto;
 
+@Entity
 public class Compra {
-
+	
+	@Id
+	@GeneratedValue
+	@Column(name="idCompra")
 	private Integer idCompra;
+	
+	@Column(name="produto")
 	private Produto produto;
 	// private Status status;
 	// private double quantidade
+	
+	
+	
+	public Integer getIdCompra() {
+		return idCompra;
+	}
+	public void setIdCompra(Integer idCompra) {
+		this.idCompra = idCompra;
+	}
+	public Produto getProduto() {
+		return produto;
+	}
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
 	
 	
 }
