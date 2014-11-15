b7800ba115e11d67c9cc25abd6c80421907d4287
Lantean <wagner258@gmail.com>
2014-11-15 00:32:52 -0200

    Correção de bugs no build path e nas anotações da classe "Compra"

--- BlackShield/src/comum/compra/entidade/Compra.java
+++ BlackShield/src/comum/compra/entidade/Compra.java
@@ -1,40 +1,91 @@
 package comum.compra.entidade;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 import org.hibernate.annotations.Entity;
 
 import comum.produto.entidade.Produto;
+import comum.status.entidade.Status;
+import comum.usuario.entidade.Usuario;
 
 @Entity
-public class Compra {
+@Table (name = "venda")
+public class Compra implements Serializable{
 	
 	@Id
 	@GeneratedValue
 	@Column(name="idCompra")
 	private Integer idCompra;
 	
-	@Column(name="produto")
+	
+	@Column (name = "quantidadeVenda")
+	private Integer quantidade;
+	
+	@ManyToOne
+	@JoinColumn (name = "idProduto")
 	private Produto produto;
-	// private Status status;
-	// private double quantidade
+	
+	@ManyToOne
+	@JoinColumn (name = "idStatus")
+	private Status status;
+	
+	@ManyToOne
+	@JoinColumn (name = "idUsuario")
+	private Usuario usuario;
+
 	
 	
 	
 	public Integer getIdCompra() {
 		return idCompra;
 	}
+
 	public void setIdCompra(Integer idCompra) {
 		this.idCompra = idCompra;
 	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
 	public Produto getProduto() {
 		return produto;
 	}
+
 	public void setProduto(Produto produto) {
 		this.produto = produto;
 	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+	
+	
+	
 	
 	
 }
